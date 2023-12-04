7f816600c3e254c4d3e6dc0ec09f84332b3aab9f
ftm2-cin <ftm2@cin.ufpe.br>
2023-12-03 22:56:28 -0300

    feat: treinamento

---
+++ client/src/pages/treinamento/index.tsx
@@ -0,0 +1,11 @@
+export default function Treinamento() {
+    const iframeStyle = {
+        width: '100%',
+        maxWidth: '100%',
+        height: '1080px'
+    };
+
+    return (
+        <iframe src="https://gamma.app/embed/w3c0instoqzx96d" style={iframeStyle} allow="fullscreen" title="Descubra o poder do React"></iframe>
+    ); 
+}
