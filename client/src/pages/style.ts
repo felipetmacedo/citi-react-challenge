14570c9cddfd2c8f0e55cfe0fa1693760f5319e1
ftm2-cin <ftm2@cin.ufpe.br>
2023-12-04 09:24:31 -0300

    style: home

--- client/src/pages/style.ts
+++ client/src/pages/style.ts
@@ -8,10 +8,11 @@ export const HomeContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background-color: white;
 
   h1 {
     font-size: 54px;
-    color: ${(props) => props.theme.colors.primary};
+    color: black;
     margin-top: 40px;
     text-align: center;
 
@@ -20,10 +21,34 @@ export const HomeContainer = styled.div`
     }
   }
 
+  h2 {
+    font-size: 36px;
+    color: black;
+    margin-top: 40px;
+    text-align: center;
+
+    @media (max-width: 720px) {
+      font-size: 24px;
+    }
+  }
+
   p {
     font-size: 20px;
     margin-top: 24px;
     line-height: 32px;
+    color: black;
+
+    @media (max-width: 720px) {
+      font-size: 16px;
+      margin-top: 0;
+    }
+  }
+
+  label {
+    font-size: 20px;
+    margin-top: 24px;
+    line-height: 32px;
+    color: black;
 
     @media (max-width: 720px) {
       font-size: 16px;
